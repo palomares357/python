cb293c6d53de3b6e18d9cd02537de31f2e31bfaa
palomares357 <82745483+palomares357@users.noreply.github.com>
2023-03-26 23:50:27 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+60 * 60
+
+
+# In[2]:
+
+
+seconds_per_hour = 60 * 60
+
+
+# In[3]:
+
+
+print(seconds_per_hour)
+
+
+# In[4]:
+
+
+print(seconds_per_hour * 24)
+
+
+# In[5]:
+
+
+seconds_per_day = seconds_per_hour * 24
+print(seconds_per_day)
+
+
+# In[6]:
+
+
+print(seconds_per_day / seconds_per_hour)
+
+
+# In[7]:
+
+
+print(seconds_per_day // seconds_per_hour)
+
+
+# In[ ]:
+
+
+
+
